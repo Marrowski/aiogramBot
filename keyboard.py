20a8eb6db72c368e6c20770b0dc90c738d6bc483
Oleh Yemelianov <emelianov2811@gmail.com>
2024-11-24 00:13:20 +0200

    Completed work

--- keyboard.py
+++ keyboard.py
@@ -1,6 +1,6 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 main_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Обрати місто')],
-                                              [KeyboardButton(text='Найпопулярніші міста')]],
+                                              [KeyboardButton(text='Допомога')]],
                                     resize_keyboard=True,
                                     input_field_placeholder='Оберіть пункт меню')
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import asyncio
-import logging
-import sys
-from os import getenv
+import requests
 
 from keyboard import main_keyboard
 from time_file import formatted_time
 
-from aiogram import Bot, Dispatcher, html, F
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
+from aiogram import Bot, Dispatcher, html, F, types
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 bot = Bot(token='7672929489:AAEO7LFKtXmdoSepJmaAgScFFRnaKMX4-M0')
 dp = Dispatcher()
+API_KEY = 'a6fb4d9c8a6d29df78711ba6ee93a935'
+
+class WeatherStates(StatesGroup):
+    waiting_for_city = State()
 
 @dp.message(CommandStart())
 async def start(message: Message):
@@ -20,12 +25,29 @@ async def start(message: Message):
     await message.answer('Обери потрібну дію.', reply_markup=main_keyboard)
 
 
-@dp.message(Command('help'))
+@dp.message(F.text == 'Допомога')
 async def help(message: Message):
     await message.answer('Це вікно допомоги.')
-    await message.answer('Щоб отримати актуальну погоду твого міста, просто введи його назву, '
-                         'або вибери із найпопулярніших')
+    await message.answer('Щоб отримати актуальну погоду твого міста, просто введи його назву')
 
+
+@dp.message(F.text == 'Обрати місто')
+async def city(message: Message, state: FSMContext):
+    await message.answer('Введіть назву міста(англійською):')
+    await state.set_state(WeatherStates.waiting_for_city)
+
+@dp.message(WeatherStates.waiting_for_city)
+async def weather_info(message: types.Message, state: FSMContext):
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={API_KEY}')
+    json_resp = response.json()
+    await message.answer('Інформація про вибране місто')
+    await message.answer('------------------------------')
+    await message.answer(f'Країна: {json_resp['sys']['country']}\n'
+          f'Місто: {json_resp['name']}\n'
+          f'Погода:{json_resp['weather'][0]['main']}\n'
+          f'Температура:{json_resp['main']['temp'] - 272}\n'
+          f'Швидкість вітру:{json_resp['wind']['speed']}')
+    await state.clear()
 
 
 async def main():
